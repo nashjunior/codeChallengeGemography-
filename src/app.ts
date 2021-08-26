949670926eb6f113bfb3011c141eb14d6c78449e
nashjunior <nashjuniorjj@gmail.com>
2021-08-25 21:39:49 -0300

    feat: app should use routes

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,9 @@
-import express from "express";
+import express from 'express';
+import routes from './routes';
+import 'express-async-errors';
 
 const app = express();
 app.use(express.json());
+app.use(routes);
 
 export default app;
